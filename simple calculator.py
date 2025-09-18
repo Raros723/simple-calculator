4d33c177ec76335ac69e063b4d73325a35fe51a0
Raros723 <zeturaris723@gmail.com>
2025-09-18 15:47:08 +0300

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -1,27 +1,26 @@
 def simple_calculator():
-    print("Bine ai venit la Calculatorul Simplu!")
-    print("Poți efectua adunări, scăderi, înmulțiri și împărțiri.")
+    print ('bun venit la simplul calculator')
+    print ('poti efectua impartiri, inmultiri si scaderi')
 
-    #calculatorul te pune sa introduci numere
+    num1= float(input('care este primul numar?'))
+    oper= input('care este operatorul?')
+    num2 = float(input('care este al doilea?'))
 
-    num1 = float(input("Introdu primul număr:"))
-    op = input ("Introdu un operator (+, -, *, /):")
-    num2 = float(input("Introdu al doilea număr:"))
+    if oper == '+':
+         print (num1 + num2)
 
-    #rezultatul
+    elif oper == '-':
+        print (num1 - num2)
+    
+    elif oper == '*':
+        print (num1 * num2)
 
-    if op == '+':
-        print ('Result:', num1 + num2)
-    elif op == '-':
-        print ('Result:', num1 - num2)
-    elif op == '*':
-        print ('Result:', num1 * num2)
-    elif op == '/':
-        if num2 > 0:
-            print ('Result:', num1 / num2)
+    elif oper == '/':
+        if num1 > 0:
+            print (num1 / num2)
+            
         else:
-            print("Eroare: Împărțirea la zero nu este permisă.")
-
+            print ('numarul este 0 sau mai mic')
 
 
 simple_calculator()
